f252cb22c317fe706bbca359d26b74f7ad102233
Emily Patrick <pat18010@byui.edu>
2019-02-25 12:17:47 -0700

    no

--- lesson-7/scripts/windchill.js
+++ lesson-7/scripts/windchill.js
@@ -1,6 +1,4 @@
-function windChill(temp, wind) {
-    var f = 35.74+(0.6215*t)-(35.75*(Math.pow(s, 0.16)))+(0.4275*t*(Math.pow(s, 0.16)));
-    var t = temp;
-    var s = wind;
-    document.getElementById("")
+function windChill() {
+    var f = 35.74+(0.6215*41)-(35.75*(Math.pow(8, 0.16)))+(0.4275*41*(Math.pow(8, 0.16)));
+    document.getElementById("windchill").innerHTML = f;
 }
