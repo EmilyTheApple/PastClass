34148129d8986c0a71c5a7b243b81f95f8393450
Emily Patrick <pat18010@byui.edu>
2019-03-12 17:08:54 -0600

    more

--- lesson-9/scripts/weatherapi.js
+++ lesson-9/scripts/weatherapi.js
@@ -1,42 +1,42 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET','EmilyTheApple.github.io\lesson-9\JSON\towndata.JSON',true)
+weatherObject.open('GET','F:/Emily/Winter%202019/EmilyTheApple.github.io/lesson-9/JSON/towndata.JSON',true)
 
 weatherObject.send();
 
 weatherObject.onload = function() {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
+    
+    document.getElementById('preston').innerhtml = weatherInfo.towns[4].name;
+    console.log(weatherInfo.towns[4].name);
+    document.getElementById('pmotto').innerhtml = weatherInfo.towns[4].motto;
+    console.log(weatherInfo.towns[4].motto);
+    document.getElementById('pyearFound').innerhtml = weatherInfo.towns[4].yearFounded;
+    console.log(weatherInfo.towns[4].yearFounded);
+    document.getElementById('pcurrentPop').innerhtml = weatherInfo.towns[4].currentPopulation;
+    console.log(weatherInfo.towns[4].currentPopulation);
+    document.getElementById('paverageRain').innerhtml = weatherInfo.towns[4].averageRainfall;
+    console.log(weatherInfo.towns[4].averageRainfall);
 
-    document.getElementById('preston').innerhtml = weatherInfo.name[4];
-    console.log(weatherInfo.name[4]);
-    document.getElementById('pmotto').innerhtml = weatherInfo.motto[4];
-    console.log(weatherInfo.motto[4]);
-    document.getElementById('pyearFound').innerhtml = weatherInfo.yearFounded[4];
-    console.log(weatherInfo.yearFounded[4]);
-    document.getElementById('pcurrentPop').innerhtml = weatherInfo.currentPopulation[4];
-    console.log(weatherInfo.currentPopulation[4]);
-    document.getElementById('paverageRain').innerhtml = weatherInfo.averageRainfall[4];
-    console.log(weatherInfo.averageRainfall[4]);
+    document.getElementById('sodasprings').innerhtml = weatherInfo.towns[5].name;
+    console.log(weatherInfo.towns[5].name);
+    document.getElementById('smotto').innerhtml = weatherInfo.towns[5].motto;
+    console.log(weatherInfo.towns[5].motto);
+    document.getElementById('syearFound').innerhtml = weatherInfo.towns[5].yearFounded;
+    console.log(weatherInfo.towns[5].yearFounded);
+    document.getElementById('scurrentPop').innerhtml = weatherInfo.towns[5].currentPopulation;
+    console.log(weatherInfo.towns[5].currentPopulation);
+    document.getElementById('saverageRain').innerhtml = weatherInfo.towns[5].averageRainfall;
+    console.log(weatherInfo.towns[5].averageRainfall);
 
-    document.getElementById('sodasprings').innerhtml = weatherInfo.name[5];
-    console.log(weatherInfo.name[5]);
-    document.getElementById('smotto').innerhtml = weatherInfo.motto[5];
-    console.log(weatherInfo.motto[5]);
-    document.getElementById('syearFound').innerhtml = weatherInfo.yearFounded[5];
-    console.log(weatherInfo.yearFounded[5]);
-    document.getElementById('scurrentPop').innerhtml = weatherInfo.currentPopulation[5];
-    console.log(weatherInfo.currentPopulation[5]);
-    document.getElementById('saverageRain').innerhtml = weatherInfo.averageRainfall[5];
-    console.log(weatherInfo.averageRainfall[5]);
-
-    document.getElementById('fishhaven').innerhtml = weatherInfo.name[1];
-    console.log(weatherInfo.name[1]);
-    document.getElementById('fmotto').innerhtml = weatherInfo.motto[1];
-    console.log(weatherInfo.motto[1]);
-    document.getElementById('fyearFound').innerhtml = weatherInfo.yearFounded[1];
-    console.log(weatherInfo.yearFounded[1]);
-    document.getElementById('fcurrentPop').innerhtml = weatherInfo.currentPopulation[1];
-    console.log(weatherInfo.currentPopulation[1]);
-    document.getElementById('faverageRain').innerhtml = weatherInfo.averageRainfall[1];
-    console.log(weatherInfo.averageRainfall[1]);
+    document.getElementById('fishhaven').innerhtml = weatherInfo.towns[1].name;
+    console.log(weatherInfo.towns[1].name);
+    document.getElementById('fmotto').innerhtml = weatherInfo.towns[1].motto;
+    console.log(weatherInfo.towns[1].motto);
+    document.getElementById('fyearFound').innerhtml = weatherInfo.towns[1].yearFounded;
+    console.log(weatherInfo.towns[1].yearFounded);
+    document.getElementById('fcurrentPop').innerhtml = weatherInfo.towns[1].currentPopulation;
+    console.log(weatherInfo.towns[1].currentPopulation);
+    document.getElementById('faverageRain').innerhtml = weatherInfo.towns[1].averageRainfall;
+    console.log(weatherInfo.towns[1].averageRainfall);
 }
