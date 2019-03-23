7e7e711ba0220cfb0a8458c5b6a3d3aa51626c83
Emily Patrick <pat18010@byui.edu>
2019-03-23 12:32:46 -0600

    ss

--- weathersitefinal/scripts/fivedayss.js
+++ weathersitefinal/scripts/fivedayss.js
@@ -1,5 +1,5 @@
 var prestonWeather = new XMLHttpRequest();
-prestonWeather.open('GET', '//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=6206458334476c0ae3b4a83d9e2c25f8&units=imperial', true);
+prestonWeather.open('GET', '//api.openweathermap.org/data/2.5/forecast?id=5607916&appid=6206458334476c0ae3b4a83d9e2c25f8&units=imperial', true);
 
 prestonWeather.send();
 
