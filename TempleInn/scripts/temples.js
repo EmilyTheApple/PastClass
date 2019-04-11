612469500dd5acbb0471cfc7569257ef296f8e3e
Emily Patrick <pat18010@byui.edu>
2019-04-10 23:37:22 -0600

    fin

--- TempleInn/scripts/temples.js
+++ TempleInn/scripts/temples.js
@@ -5,5 +5,41 @@ templeObject.send();
 
 templeObject.onload = function() {
     var templeInfo = JSON.parse(templeObject.responseText);
-    console.log(templeInfo) 
+    console.log(templeInfo);
+
+    document.getElementById('mcname').innerHTML = 
+    document.getElementById('mcaddress').innerHTML =
+    document.getElementById('mctelephone').innerHTML =
+    document.getElementById('mcservices').innerHTML =
+    document.getElementById('mchistory').innerHTML =
+    document.getElementById('mcordinances').innerHTML =
+    document.getElementById('mcsessions').innerHTML =
+    document.getElementById('mcclosure').innerHTML =
+
+    document.getElementById('vbcname').innerHTML =
+    document.getElementById('vbcaddress').innerHTML =
+    document.getElementById('vbctelephone').innerHTML =
+    document.getElementById('vbcservices').innerHTML =
+    document.getElementById('vbchistory').innerHTML =
+    document.getElementById('vbcordinances').innerHTML =
+    document.getElementById('vbcsessions').innerHTML =
+    document.getElementById('vbcclosure').innerHTML =
+    
+    document.getElementById('mnyname').innerHTML =
+    document.getElementById('mnyaddress').innerHTML =
+    document.getElementById('mnytelephone').innerHTML =
+    document.getElementById('mnyservices').innerHTML =
+    document.getElementById('mnyhistory').innerHTML =
+    document.getElementById('mnyordinances').innerHTML =
+    document.getElementById('mnysessions').innerHTML =
+    document.getElementById('mnyclosure').innerHTML =
+
+    document.getElementById('fgtname').innerHTML =
+    document.getElementById('fgtaddress').innerHTML =
+    document.getElementById('fgttelephone').innerHTML =
+    document.getElementById('fgtservices').innerHTML =
+    document.getElementById('fgthistory').innerHTML =
+    document.getElementById('fgtordinances').innerHTML =
+    document.getElementById('fgtsessions').innerHTML =
+    document.getElementById('fgtclosure').innerHTML =
 }
