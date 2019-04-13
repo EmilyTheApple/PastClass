35cad76d37c8d44c08a91f3304795b81e8583006
Emily Patrick <pat18010@byui.edu>
2019-04-12 23:21:21 -0600

    temp

---
+++ TempleInn/scripts/mc.js
@@ -0,0 +1,13 @@
+var mexicoWeather = new XMLHttpRequest();
+mexicoWeather.open('GET', '//api.openweathermap.org/data/2.5/weather?id=3530597&appid=6206458334476c0ae3b4a83d9e2c25f8&units=imperial', true);
+
+mexicoWeather.send();
+
+mexicoWeather.onload = function() {
+    var weatherInfo = JSON.parse(mexicoWeather.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('mccon').innerHTML = weatherInfo.list["0"].weather["0"].main;
+    document.getElementById('mclow').innerHTML = weatherInfo.list["0"].main.temp_min;
+    document.getElementById('mchi').innerHTML = weatherInfo.list["0"].main.temp_max;
+}
---
+++ TempleInn/scripts/mny.js
@@ -0,0 +1,14 @@
+var manhattanWeather = new XMLHttpRequest();
+manhattanWeather.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5585010&appid=6206458334476c0ae3b4a83d9e2c25f8&units=imperial', true);
+
+manhattanWeather.send();
+
+manhattanWeather.onload = function() {
+    var weatherInfo = JSON.parse(manhattanWeather.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('currcon').innerHTML = weatherInfo.list["0"].weather["0"].main;
+    document.getElementById('currlow').innerHTML = weatherInfo.list["0"].main.temp_min;
+    document.getElementById('currhi').innerHTML = weatherInfo.list["0"].main.temp_max;
+    document.getElementById('currws').innerHTML = weatherInfo.list["0"].wind.speed;
+}
